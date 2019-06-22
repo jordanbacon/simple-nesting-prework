b42648cec0502bd17c1a61225f7e67ade4ba15e0
Jordan Bacon <jmbacon92@gmail.com>
2019-06-22 07:42:09 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,7 @@ def dennis_ritchies_language
         }
      }
 languages_string = programmer_hash[:dennis_ritchie][:languages]
-languages_string.to_s
+languages_string.to_str
 end
 
 def adding_matz
